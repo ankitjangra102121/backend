e99093e217ff9b872defe836b1992c0e1826ac01
Ankit Jangra <ankitjangra102121@gmail.com>
2026-03-24 02:07:33 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -7,6 +7,34 @@ import connectDB from "./db/index.js"
 
 
 connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server started at PORT - ${process.env.PORT}`);
+    })
+})
+.catch( (err) => {
+    console.log("MongoDB connection failed!!!", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
